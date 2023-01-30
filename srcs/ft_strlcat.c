f3649645d2a061a3b8c3e2c7981b2267becff2fe
taku-arashiro <apndx19@gmail.com>
2023-01-30 17:49:45 +0900

    modifying a file ft_strlcat.c

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -1,22 +1,35 @@
 #include	<stddef.h>
 
+int	ft_strlen(char * s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
 size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
 {
 	char	*p1;
 	char	*p2;
 	size_t	len;
+	size_t	return_val;
 
-	len = 0;
 	p1 = (char*)dst;
 	p2 = (char*)src;
-	while (p1[len])
-		len++;
-	while (*p2 && len < dstsize)
+	len = ft_strlen(p1);
+
+	if (dstsize < len)
+		return (ft_strlen(p2) + dstsize);
+	return_val = len + ft_strlen(p2);
+	while (*p2 && len < dstsize - 1)//dstsize is 1 + len(dst)
 	{
 		p1[len] = *p2;
 		p2++;	
 		len++;
 	}
 	p1[len] = '\0';
-	return (len);
+	return (return_val);
 }
