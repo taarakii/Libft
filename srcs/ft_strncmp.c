c59a92cb92c0ac10fc4d91e22d65c48ec1c57cf2
taku-arashiro <apndx19@gmail.com>
2023-01-31 22:23:01 +0900

    modifying ft_strncmp.c

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -8,9 +8,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		if (*s1 != *s2)
 		{
 			if (*s2 < 0)
-				return (-128 + *s1 + (-128 - *s2));
-			//printf("s1:%d\n", *s1);
-			//printf("s2:%d\n", *s2);
+				return (*s1 - *s2 - 256);
 			return (*s1 - *s2);
 		}
 		s1++;
