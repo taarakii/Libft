72780973d778e211dafc7450e7c50b406550f5f9
taku-arashiro <apndx19@gmail.com>
2023-01-31 22:19:54 +0900

    modifying ft_strncmp.c

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -1,20 +1,21 @@
-#include <stddef.h>
+#include	<stddef.h>
+#include	<stdio.h>
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while (n > 0)
 	{
 		if (*s1 != *s2)
+		{
+			if (*s2 < 0)
+				return (-128 + *s1 + (-128 - *s2));
+			//printf("s1:%d\n", *s1);
+			//printf("s2:%d\n", *s2);
 			return (*s1 - *s2);
+		}
 		s1++;
 		s2++;
 		n--;
 	}
-//	if (n == 0)
-		return (0);
-/*
-	if (*s1 == 0 || *s2 == 0)
-		return (*s1 - *s2);
 	return (0);
-*/
 }
