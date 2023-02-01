73ff7bfb57da1315f5b4f6812bc197a512cb654d
taku-arashiro <apndx19@gmail.com>
2023-02-01 12:37:12 +0900

    modifying ft_strncstr.c

---
+++ strnstr_d/test.c
@@ -0,0 +1,15 @@
+#include	<stdio.h>
+#include	<string.h>
+
+char	*ft_strnstr(const char *haystatck, const char *needle, size_t len);
+
+int	main(void)
+{
+        char *str = "libft-test-tokyo";
+        int     len = strlen(str);
+        for (int i = 0; i <= len; i++)
+        {
+                printf("%d:'%s' '%s'\n", i, ft_strnstr(str, "libft-test-tokyo", i), strnstr(str, "libft-test-tokyo", i));	
+	}
+	return (0);
+}
