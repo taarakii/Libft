3d3dec19f5dc0fabdb924bf7dc68cbdf7cdcf017
taku-arashiro <apndx19@gmail.com>
2023-02-01 16:16:14 +0900

    |ft_atoi.c| optimizing the length of the function

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -1,5 +1,4 @@
 #include	"libft.h"
-#include	<stdio.h>
 
 int	ft_is_whitespace(char c)
 {
@@ -17,13 +16,6 @@ int	ft_strlen_num(char *s)
 	while(s[len] >= '0' && s[len] <= '9')
 		len++;
 	return (len);
-}
-
-int	ft_is_zero(char c)
-{
-	if (c == '0')
-		return (1);
-	return (0);
 }
 
 //@param (str, flag, sign)
@@ -71,69 +63,30 @@ int	ft_atoi(const char *str)
 	char			*p;
 	int			flag;
 	int			sign;
-	int			len;
+	//int			len;
 	int			i;
 	unsigned long long	x;
 
-	//@param (str, flag, sign)
-	//skip whitespaces, check sign
 	flag = 0;
 	sign = 1;
-/*
-	p = (char*)str;	
-	while (ft_is_whitespace(*p))
-		p++;
-	if (*p == '+')
-	{
-		flag = 1;
-		p++;
-	}
-	else if (*p == '-')
-	{
-		flag = 1;
-		sign = -1;
-		p++;
-	}
-*/
+	/* OPERATION ONE */
 	p = operation_one((char*)str, &flag, &sign);
-	//
 	if (flag == 1 && !(*p >= '0' && *p <= '9'))
 		return (0);
-	while (ft_is_zero(*p))
+	while (*p == '0')
 		p++;
-	len = ft_strlen_num(p);	
-	if ((len > 20 && sign == 1) || ft_strcmp(p, "18446744073709551616") == 0)
+	//len = ft_strlen_num(p);	
+	if ((ft_strlen_num(p) > 20 && sign == 1) || ft_strcmp(p, "18446744073709551616") == 0)
 		return (-1);
-	else if (len > 20 && sign == -1)
+	else if (ft_strlen_num(p) > 20 && sign == -1)
 		return (1);
-	x = 0;
-	i = 0;
-	while (i < len)
-	{
-		x = (x * 10) + (p[i] - '0');
-		i++;
-	}
-
 	if ((!ft_strcmp(p, "9223372036854775809") || !ft_strcmp(p, "9223372036854775808")) && sign == -1)
 		return (0);
-/*
-	//
-	if (x == 2147483648 && sign == 1)
-		return (-2147483648);
-	else if (x == 2147483649 && sign == -1)
-		return (2147483647);
-	else if (x == 4294967294 || x == 9223372036854775806)
-		return (-2);
-	else if (x == 4294967296)// || ((!ft_strcmp(p, "9223372036854775809") || !ft_strcmp(p, "9223372036854775808")) && sign == -1))
-		return (0);
-	else if (x > 2147483648 && sign == -1)
-		return (1);
-	else if (x > 2147483647 && sign == 1)
-		return (-1);
-	else
-		return ((int)(sign * x));
-	//
-*/
+	x = 0;
+	i = 0;
+	while (i < ft_strlen_num(p))
+		x = (x * 10) + (p[i++] - '0');
+	/* OPERATION TWO */
 	return (operation_two(x, sign));
 }
 
