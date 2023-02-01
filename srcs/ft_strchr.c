539f20d45e84c71479f266367ad594dc7fa2edff
taku-arashiro <apndx19@gmail.com>
2023-02-01 12:00:07 +0900

    modifying ft_strchr.c

--- srcs/ft_strchr.c
+++ srcs/ft_strchr.c
@@ -1,15 +1,15 @@
-char	*ft_strchr(const char *s, char c)
+char	*ft_strchr(const char *s, int c)
 {
 	char	*p;
 
 	p = (char *)s;
 	while (*p)
 	{
-		if (*p == c)
+		if (*p == (char)c)
 			return (p);
 		p++;
 	}
-	if (*p == c)
+	if (*p == (char)c)
 		return (p);
 	return (0);
 }
