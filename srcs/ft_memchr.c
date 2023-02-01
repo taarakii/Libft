8a2c80dbbfeca59c870b5ef4c65bc68ca59c8f09
taku-arashiro <apndx19@gmail.com>
2023-02-01 11:58:56 +0900

    modifying ft_strncmp.c ft_memchr.c

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -2,12 +2,14 @@
 
 void *ft_memchr(const void *s, int c, size_t n)
 {
-	char	*p;	
+	char		*p;	
+	unsigned char	check;
 
+	check = (unsigned char)c;
 	p = (char*)s;
 	while (*p && n > 0)
 	{
-		if (*p == c)
+		if ((unsigned char)*p == check)
 			return ((void*)p);
 		p++;
 		n--;
--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,11 +6,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (n > 0)
 	{
 		if (*s1 != *s2)
-		{
-			if (*s2 < 0)
-				return (*s1 - *s2 - 256);
-			return (*s1 - *s2);
-		}
+			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
 		n--;
