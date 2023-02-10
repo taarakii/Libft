9153f3a4c0dd1ce535ca3a15b19aca3d776da381
Takumi Araki <apndx19@gmail.com>
2023-02-10 12:08:45 +0900

    adding new files ft_itoa.c

---
+++ part2/itoa_d/ft_itoa.c
@@ -0,0 +1,20 @@
+#include	<stdlib.h>
+//#include	"libft.h"
+
+//@param
+// n: the integer to convert.
+//@return_val
+// The string representing the integer. 
+// NULL if the allocation fails.
+
+//@description
+// Allocates and returns a string repreesenting 
+// the integer received as an argument.
+// Negative numbers must be handled.
+
+char	*ft_itoa(int n)
+{
+	char	*p;
+
+	return (p);
+}
