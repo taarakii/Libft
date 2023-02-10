6fdbcb98c12037569f84221554524e8673604a80
Takumi Araki <apndx19@gmail.com>
2023-02-10 16:28:53 +0900

    modyfing ft_itoa.c

--- part2/itoa_d/test.c
+++ part2/itoa_d/test.c
@@ -4,22 +4,31 @@ char	*ft_itoa(int n);
 
 int	main(void)
 {
-	/* 1 */ printf("%s", ft_itoa(0));
-	/* 2 */ printf("%s", ft_itoa(-0));
-	/* 3 */ printf("%s", ft_itoa(+0));
-	/* 4 */ printf("%s", ft_itoa(1));
-	/* 5 */ printf("%s", ft_itoa(-1));
-	/* 6 */ printf("%s", ft_itoa(+1));
-	/* 7 */ printf("%s", ft_itoa(7));
-	/* 8 */ printf("%s", ft_itoa(-7));
-	/* 9 */ printf("%s", ft_itoa(+7));
-	/* 10 */ printf("%s", ft_itoa(42));
-	/* 11 */ printf("%s", ft_itoa(-42));
-	/* 12 */ printf("%s", ft_itoa(+42));
-	/* 13. INT_MAX */ printf("%s", ft_itoa(2147483647));
-	/* 14. INT_MIN */ printf("%s", ft_itoa(-2147483648));
-	/* 15. INT_MAX - 1 */ printf("%s", ft_itoa(2147483646));
-	/* 16. INT_MIN + 1 */ printf("%s", ft_itoa(-2147483647));
+	char *p;
+	//printf("%s\n", ft_itoa(-2147483648));
+	p = ft_itoa(111);
+	printf("p:%s\n", p);
+
+	//printf("%s\n", ft_itoa(8));
+	//printf("%s\n", ft_itoa(188));
+
+
+//	/* 1 */ printf("%s\n", ft_itoa(0));
+//	/* 2 */ printf("%s\n", ft_itoa(-0));
+//	/* 3 */ printf("%s\n", ft_itoa(+0));
+//	/* 4 */ printf("%s\n", ft_itoa(1));
+//	/* 5 */ printf("%s\n", ft_itoa(-1));
+//	/* 6 */ printf("%s\n", ft_itoa(+1));
+//	/* 7 */ printf("%s\n", ft_itoa(7));
+//	/* 8 */ printf("%s\n", ft_itoa(-7));
+//	/* 9 */ printf("%s\n", ft_itoa(+7));
+//	/* 10 */ printf("%s\n", ft_itoa(42));
+//	/* 11 */ printf("%s\n", ft_itoa(-42));
+//	/* 12 */ printf("%s\n", ft_itoa(+42));
+//	/* 13. INT_MAX */ printf("%s\n", ft_itoa(2147483647));
+//	/* 14. INT_MIN */ printf("%s\n", ft_itoa(-2147483648));
+//	/* 15. INT_MAX - 1 */ printf("%s\n", ft_itoa(2147483646));
+//	/* 16. INT_MIN + 1 */ printf("%s\n", ft_itoa(-2147483647));
 
 	return (0);
 }
