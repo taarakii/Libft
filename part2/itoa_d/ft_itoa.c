9cc2b38a17e4dc2a7914b3b63a4695d7b8c66346
Takumi Araki <apndx19@gmail.com>
2023-02-10 16:01:30 +0900

    modifying ft_itoa.c

--- part2/itoa_d/ft_itoa.c
+++ part2/itoa_d/ft_itoa.c
@@ -12,9 +12,57 @@
 // the integer received as an argument.
 // Negative numbers must be handled.
 
+char	*ft_itoa(int n);
+int	int_len(int n);
+
+int	int_len(int n)
+{
+	int	i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		n = 10;
+		i++;
+	}
+	return (i);
+}
+
+
 char	*ft_itoa(int n)
 {
+	char	s[12];
 	char	*p;
+	int	flag;//flag for whether the number is negative or not
+	int	len;
+	int	i;
 
-	return (p);
+	// set the flag to be 0
+	flag = 0;
+	// handle negative values
+	if (n == -2147483648)
+		return ("-2147483648");
+	else if (n < 0)
+	{
+		n = -n;
+		//*p = '-';
+		s[0] = '-';
+		flag = 1;
+		//p++;
+	}
+
+	//count the number of digits of an integer
+	len =  int_len(n);// + flag;
+	i = 0;
+	while (i < len)
+	{
+		str[len + flag - i - 1] = (n % 10) + '0';
+		n /= 10;
+		i++;
+	}		
+	while (i <= len)
+		str[i++] = '\0';
+	return (str);
 }
