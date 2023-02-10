34292c489198beeba46a45e909843e2af6b7697f
Takumi Araki <apndx19@gmail.com>
2023-02-10 16:34:15 +0900

    modyfing ft_itoa.c

--- part2/itoa_d/ft_itoa.c
+++ part2/itoa_d/ft_itoa.c
@@ -1,6 +1,5 @@
-#include	<stdio.h>
 #include	<stdlib.h>
-//#include	"libft.h"
+#include	"libft.h"
 
 int	int_len(int n)
 {
@@ -21,12 +20,11 @@ int	int_len(int n)
 char	*ft_itoa(int n)
 {
 	char	*p;
-	int	flag;//flag for whether the number is negative or not
+	int	flag;
 	int	len;
 	int	i;
 
 	flag = 0;
-	// handle negative values
 	if (n == -2147483648)
 		return ("-2147483648");
 	else if (n < 0)
@@ -34,18 +32,15 @@ char	*ft_itoa(int n)
 		n = -n;
 		flag = 1;
 	}
-	//count the number of digits of an integer
 	len =  int_len(n);
-	//dynamically allocate the memory with malloc
 	p = (char*)malloc((len + flag + 1) + sizeof(char));
 	if (flag == 1)
 		*p = '-';
 	i = 0;
 	while (i < len)
 	{
-		p[len + flag - i - 1] = (n % 10) + '0';
+		p[len + flag - i++ - 1] = (n % 10) + '0';
 		n /= 10;
-		i++;
 	}		
 	p[len + flag] = '\0';
 	return (p);
