e786a13bc8e4c28f89364468c9d6b7a6d3c24995
Takumi Araki <apndx19@gmail.com>
2023-02-12 17:33:36 +0900

    adding files for ft_striter.c

--- includes/libft.h
+++ includes/libft.h
@@ -2,6 +2,7 @@
 # define	_LIBFT_H_
 
 #include <stddef.h>
+#include <stdlib.h>
 
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
@@ -31,5 +32,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 # endif
---
+++ part2/striteri_d/test.c
@@ -0,0 +1,9 @@
+#include	<stdio.h>
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+int	main(void)
+{
+
+	return (0);
+}
