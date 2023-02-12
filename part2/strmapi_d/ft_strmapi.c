52f12495bc1282c1cd1b0d0a2caae29b7d52b588
Takumi Araki <apndx19@gmail.com>
2023-02-12 17:28:23 +0900

    modyfing ft_strmapi.c

--- part2/strmapi_d/ft_strmapi.c
+++ part2/strmapi_d/ft_strmapi.c
@@ -1,21 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 17:16:27 by taaraki           #+#    #+#             */
+/*   Updated: 2023/02/12 17:27:36 by taaraki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include	<stdlib.h>
 #include	"libft.h"
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char		*p;
-	unsigned int	len;
-	unsigned int	i;
-	
+	char	*p;
+	size_t	len;
+	size_t	i;
+
 	len = 0;
-	len = (unsigned int)ft_strlen(s);
-	p = (char*)malloc((len + 1) * sizeof(char) + 1);
+	len = ft_strlen(s);
+	p = (char *)malloc((len + 1) * sizeof(char) + 1);
 	if (p == NULL)
 		return (NULL);
 	i = 0;
 	while (i < len)
 	{
-		p[i] = (*f)(len, s[i]);
+		p[i] = (*f)(i, s[i]);
 		i++;
 	}
 	return (p);
--- srcs/ft_strmapi.c
+++ srcs/ft_strmapi.c
@@ -1,32 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 17:16:27 by taaraki           #+#    #+#             */
+/*   Updated: 2023/02/12 17:27:36 by taaraki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include	<stdlib.h>
 #include	"libft.h"
 
-//@param
-// s: The string on which to iterate.
-// f: The function to apply to each character.
-//@return_val
-// The string created from the successive applications of 'f'
-// Returns NULL if the allocation fails.
-//@description
-// Applies the function 'f' to each character of the string 's',
-// and passing its index as first argument to create a new string
-// resulting from successive applications of 'f'.
-
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char		*p;
-	unsigned int	len;
-	unsigned int	i;
-	
+	char	*p;
+	size_t	len;
+	size_t	i;
+
 	len = 0;
-	len = (unsigned int)ft_strlen(s);
-	p = (char*)malloc((len + 1) * sizeof(char) + 1);	
+	len = ft_strlen(s);
+	p = (char *)malloc((len + 1) * sizeof(char) + 1);
 	if (p == NULL)
 		return (NULL);
 	i = 0;
 	while (i < len)
 	{
-		p[i] = (*f)(len, s[i]);
+		p[i] = (*f)(i, s[i]);
 		i++;
 	}
 	return (p);
