6cb81e1d253ec7930d7d9834f37836c6926359eb
Takumi Araki <apndx19@gmail.com>
2023-02-13 17:41:56 +0900

    completed ft_strdup.c

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -6,15 +6,22 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 17:23:36 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/13 17:24:17 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/02/13 17:39:22 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	<stdlib.h>
+#include	"libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s)
 {
 	char	*p;
+	size_t	len;
 
+	len = ft_strlen(s);
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	ft_strlcpy(p, s, len + 1);
 	return (p);
 }
