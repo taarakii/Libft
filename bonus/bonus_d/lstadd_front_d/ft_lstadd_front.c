f59eab1c232a2694e4cd812bebdc8e7b17c869fc
Takumi Araki <apndx19@gmail.com>
2023-02-15 17:06:34 +0900

    adding new files, ft_lstadd_front.c

---
+++ bonus/bonus_d/lstadd_front_d/ft_lstadd_front.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 17:04:50 by taaraki           #+#    #+#             */
+/*   Updated: 2023/02/15 17:06:14 by taaraki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+typedef struct	s_list
+{
+ void		*content;
+ struct s_list	*next;
+}		t_list;
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+
+
+}
