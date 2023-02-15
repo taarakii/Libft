6a31bad4d8904ebe921afff4419c0d2fa410e03b
Takumi Araki <apndx19@gmail.com>
2023-02-15 17:35:50 +0900

    completed ft_lstadd_front.c

--- bonus/ft_lstadd_front.c
+++ bonus/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 17:04:50 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/15 17:31:28 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/02/15 17:35:02 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,17 @@ typedef struct	s_list
 */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	**temp;
+	//t_list	**temp;
+	t_list	*temp;
 
 	if (new == NULL)
 		return ;
-	temp = lst;
+	//temp = lst;
+	temp = *lst;
 	//I think this right here is correct.
 	*lst = new;
-	//(*lst)->next = temp;
-	(*lst)->next = *temp;
+	//(*lst)->next = *temp;
+	(*lst)->next = temp;
 	//lst++;
 	//lst = temp;
 }
