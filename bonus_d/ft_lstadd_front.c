d42c9b71f8dd2bda2ebbf20a6680ae081d3ef9aa
Takumi Araki <apndx19@gmail.com>
2023-02-15 17:32:15 +0900

    modifying files for ft_lstadd_front.c

---
+++ bonus_d/ft_lstadd_front.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 17:04:50 by taaraki           #+#    #+#             */
+/*   Updated: 2023/02/15 17:31:28 by taaraki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+/*
+typedef struct	s_list
+{
+ void		*content;
+ struct s_list	*next;
+}		t_list;
+*/
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	t_list	**temp;
+
+	if (new == NULL)
+		return ;
+	temp = lst;
+	//I think this right here is correct.
+	*lst = new;
+	//(*lst)->next = temp;
+	(*lst)->next = *temp;
+	//lst++;
+	//lst = temp;
+}
--- bonus_d/lstadd_front_d/ft_lstadd_front.c
+++ bonus_d/lstadd_front_d/ft_lstadd_front.c
@@ -6,20 +6,21 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 17:04:50 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/15 17:06:14 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/02/15 17:10:16 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
-
+/*
 typedef struct	s_list
 {
  void		*content;
  struct s_list	*next;
 }		t_list;
-
+*/
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-
-
+	if (new == NULL)
+		return ;
+	*lst = new;
 }
