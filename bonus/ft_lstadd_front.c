76f1d7bf7e4a4a508cf873bc36892d8fa15a2466
Takumi Araki <apndx19@gmail.com>
2023-02-15 17:38:20 +0900

    modifyig files (ft_lstadd_front.c)

--- bonus/ft_lstadd_front.c
+++ bonus/ft_lstadd_front.c
@@ -6,31 +6,19 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 17:04:50 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/15 17:35:02 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/02/15 17:37:04 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
-/*
-typedef struct	s_list
-{
- void		*content;
- struct s_list	*next;
-}		t_list;
-*/
+
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	//t_list	**temp;
 	t_list	*temp;
 
 	if (new == NULL)
 		return ;
-	//temp = lst;
 	temp = *lst;
-	//I think this right here is correct.
 	*lst = new;
-	//(*lst)->next = *temp;
 	(*lst)->next = temp;
-	//lst++;
-	//lst = temp;
 }
--- bonus_d/lstadd_front_d/ft_lstadd_front.c
+++ bonus_d/lstadd_front_d/ft_lstadd_front.c
@@ -6,21 +6,19 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 17:04:50 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/15 17:10:16 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/02/15 17:37:04 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
-/*
-typedef struct	s_list
-{
- void		*content;
- struct s_list	*next;
-}		t_list;
-*/
+
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	t_list	*temp;
+
 	if (new == NULL)
 		return ;
+	temp = *lst;
 	*lst = new;
+	(*lst)->next = temp;
 }
