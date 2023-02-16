7a5e148b0afe7f74059c65fa1d2fe7094da75a38
Takumi Araki <apndx19@gmail.com>
2023-02-16 17:36:03 +0900

    completed ft_lstsize.c

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = cc
 CFLAGS = -Wall -Wextra -Werror
 FILES =ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_toupper.c ft_tolower.c ft_strlen.c ft_strchr.c ft_strrchr.c ft_strncmp.c ft_strcmp.c ft_memset.c ft_memchr.c ft_memcpy.c ft_memmove.c ft_memcmp.c ft_bzero.c ft_strlcpy.c ft_strlcat.c ft_strnstr.c ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
-BONUS =ft_lstnew.c ft_lstadd_front.c
+BONUS =ft_lstnew.c ft_lstadd_front.c ft_lstsize.c
 PRE = ./srcs/
 PRE_BONUS = ./bonus/
 SRCS = $(addprefix $(PRE), $(FILES))
---
+++ bonus_d/ft_lstsize.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 17:10:10 by taaraki           #+#    #+#             */
+/*   Updated: 2023/02/16 17:30:43 by taaraki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"libft.h"
+
+typedef struct	s_list
+{
+ void		*content;
+ struct s_list	*next;
+}		t_list;
+
+int	ft_lstsize(t_list *lst)
+{
+	size_t	i;
+
+	i = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		i++;
+	}
+	if (i + (unsigned int)1 < i)
+		return (-1);
+	return (i);
+}
