af92836c9a1cad052fa4fabb07250e4706c6d47e
Takumi Araki <apndx19@gmail.com>
2023-02-18 17:09:44 +0900

    completed ft_lstadd_back.c

--- bonus/ft_lstadd_back.c
+++ bonus/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 17:46:48 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/17 21:51:21 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/02/18 17:08:30 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,20 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 
 	//increment the pointer until the last element
 	int i = 0;
-	while ((*lst)->next != NULL)
+	//while ((*lst)->next != NULL)
+	while ((*lst + i)->next != NULL)
 	{
 		printf("\ni:%d ", i);
-		*lst = (*lst)->next;
+		//*lst = (*lst)->next;
 		i++;
 	}
 
 	//add the new pointer
-	(*lst)->next = new;
+	//(*lst)->next = new;
+	(*lst + i)->next = new;
+
+	//checking the previous list (Feb18 5:05PM)
+	//printf("\n%s ", (char *)((*lst - 1)->content));//this way of accessing a list with a pointer is possible
 	printf("\n%s ", (char *)((*lst)->content));
 	if (((*lst)->next) != NULL)
 		printf("\nnext is not null");
