4f58e19c1dedcdf13d732209ed69905965c02048
Takumi Araki <apndx19@gmail.com>
2023-02-18 17:12:08 +0900

    adding files, ft_lsadd_back.c (leaving some comments. there're a few thing I need to check)

--- bonus_d/ft_lstadd_back.c
+++ bonus_d/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 17:46:48 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/18 17:08:30 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/02/18 17:11:03 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	size_t	i;
 
 	if (new == NULL)
 	{
@@ -32,31 +33,22 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	//	return ;
 	if (*lst == NULL)
 	{
-		printf("\n*lst is null");
 		*lst = new;
 		return ;
 	}
 
 	//increment the pointer until the last element
-	int i = 0;
+	i = 0;
 	//while ((*lst)->next != NULL)
 	while ((*lst + i)->next != NULL)
 	{
-		printf("\ni:%d ", i);
 		//*lst = (*lst)->next;
 		i++;
 	}
 
 	//add the new pointer
-	//(*lst)->next = new;
 	(*lst + i)->next = new;
 
-	//checking the previous list (Feb18 5:05PM)
-	//printf("\n%s ", (char *)((*lst - 1)->content));//this way of accessing a list with a pointer is possible
-	printf("\n%s ", (char *)((*lst)->content));
-	if (((*lst)->next) != NULL)
-		printf("\nnext is not null");
-	printf("\n%s ", (char *)((*lst)->next->content));
 	//set NULL at the last element after adding the pointer
 	//(*lst + 1)->next = NULL;
 }
