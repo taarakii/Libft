2d45fe13e639ee64094c702655a3344ef00d5df0
Takumi Araki <apndx19@gmail.com>
2023-03-12 14:07:49 +0900

    modifying ft_itoa.c

--- assignment_libft/ft_itoa.c
+++ assignment_libft/ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 15:22:31 by taaraki           #+#    #+#             */
-/*   Updated: 2023/03/03 16:49:05 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/03/12 14:07:32 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 
-static int	int_len(int n)
+static size_t	int_len(int n)
 {
 	int	i;
 
@@ -27,23 +27,10 @@ static int	int_len(int n)
 	return (i);
 }
 
-char	*ft_itoa(int n)
+static char	*convert_int(int n, char *p, size_t len, size_t neg_flag)
 {
-	char	*p;
-	int		neg_flag;
-	int		len;
-	int		i;
+	size_t	i;
 
-	neg_flag = 0;
-	if (n == -2147483648)
-		return ("-2147483648");
-	else if (n < 0)
-	{
-		n = -n;
-		neg_flag = 1;
-	}
-	len = int_len(n);
-	p = (char *)malloc((len + neg_flag + 1) + sizeof(char));
 	if (neg_flag == 1)
 		*p = '-';
 	i = 0;
@@ -51,7 +38,31 @@ char	*ft_itoa(int n)
 	{
 		p[len + neg_flag - i++ - 1] = (n % 10) + '0';
 		n /= 10;
-	}		
+	}
 	p[len + neg_flag] = '\0';
+	return (p);
+}
+
+//neg_flag is for checking if the value is negative
+//and securing an extra memory at the begining of the string.
+char	*ft_itoa(int n)
+{
+	char	*p;
+	size_t	neg_flag;
+	size_t	len;
+
+	neg_flag = 0;
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	else if (n < 0)
+	{
+		n = -n;
+		neg_flag = 1;
+	}
+	len = int_len(n);
+	p = (char *)malloc(sizeof(char) * (len + neg_flag + 1));
+	if (p == NULL)
+		return (NULL);
+	p = convert_int(n, p, len, neg_flag);
 	return (p);
 }
